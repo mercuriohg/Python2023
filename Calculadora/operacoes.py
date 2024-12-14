f15e57ed0813ace0dfea0ce4cb2350535e23e924
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2024-12-14 20:59:50 -0300

    Update operacoes.py

--- Calculadora/operacoes.py
+++ Calculadora/operacoes.py
@@ -1,1 +1,26 @@
 
+import math
+
+def soma(a,b):
+    return a + b 
+
+def divisao(a,b):
+    return a / b
+
+def mult(a,b):
+    return a * b 
+
+def sub(a,b):
+    return a - b 
+
+def raizquadrada(a):
+    return a 
+
+def sin(a):
+    return a 
+
+def cos(a):
+    return a 
+
+def tg(a):
+    return a 
