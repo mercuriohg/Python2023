83f633973149540933204c2657cc59c047b63dcd
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2024-12-14 15:18:51 -0300

    Update moeda.py

--- Conversor de moedas/moeda.py
+++ Conversor de moedas/moeda.py
@@ -1,1 +1,21 @@
+def dolar(a):
+    return a * 0.2
+    dolar(valor)
 
+def euro(a):
+    return a * 0.19
+    euro(valor)
+
+def libra(a):
+    return a * 0.16
+    libra(valor)  
+
+def iene(a):
+    return a * 30.29
+    iene(valor)
+
+def titulo(msg):
+    tam = len(msg)
+    print('=+=' * tam)
+    print(  msg)
+    print('=+=' * tam)
