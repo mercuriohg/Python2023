eb4dc88446d85f425b2d57b53959dc19912c2982
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-01-06 22:10:57 -0300

    Update roletarussa.py

--- Roleta Russa/roletarussa.py
+++ Roleta Russa/roletarussa.py
@@ -1,1 +1,22 @@
-'
+import random
+
+
+desc = [0,0,0,0,0,1]
+random.shuffle(desc)
+tentativas = 0
+while desc:
+        input("Aperte o Enter para atirar: ")
+        resul = desc.pop()
+
+        if resul == 1:
+           print("POW!")
+           print("Você morreu!")
+           break
+        else: 
+            print("Você tem mais uma tentativa")
+            tentativas += 1
+            
+         
+
+print(f"Teve {tentativas} tentativas")
+print("FIM")
