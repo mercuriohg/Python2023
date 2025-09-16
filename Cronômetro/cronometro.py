c4a021af0b793fc7934e0ee0354778e0918f6aba
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-09-16 11:09:28 -0300

    Update cronometro.py

--- Cronômetro/cronometro.py
+++ Cronômetro/cronometro.py
@@ -5,6 +5,4 @@ for i in range(1,numS+1):
     hora = i//3600
     min = (i%3600)//60
     seg = i%60
-    print(f'{hora:.0f}h: {min:.0f}min: {seg}seg')
-
-    
+    print(f'{hora:.0f}h: {min:.0f}min: {seg}seg')    
